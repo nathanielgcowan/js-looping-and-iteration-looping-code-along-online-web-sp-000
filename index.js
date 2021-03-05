77c4002fcc68b49161ec6dd215a93386584fd2e4
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2021-03-05 03:25:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solutions in this file
+const gifts = ['Lisa', 'Kaitlin', 'Jan' ];
+
+function writeCards(gifts) {
+  let i = 0; // the initialization moves OUTSIDE the body of the loop!
+  while (i < gifts.length) {
+    console.log(`Thank you, ${gifts[i]} for the wonderful birthday card!`);
+    i++; // the iteration moves INSIDE the body of the loop!
+  }
+
+  return gifts;
+}
+
+writeCards(gifts);
