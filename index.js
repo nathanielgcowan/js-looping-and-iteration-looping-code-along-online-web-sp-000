d5193f6013961051278f03f402bda1a802457a86
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2021-03-06 03:44:14 +0000

    Done.

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-const gifts = ['Lisa', 'Kaitlin', 'Jan' ];
-
-function writeCards(gifts) {
-  let i = 0; // the initialization moves OUTSIDE the body of the loop!
-  while (i < gifts.length) {
-    console.log(`Thank you, ${gifts[i]} for the wonderful birthday card!`);
-    i++; // the iteration moves INSIDE the body of the loop!
-  }
-
-  return gifts;
+function writeCards( names, event ){
+  let thankYouCards = []
+  for ( let i = 0; i < names.length; i++ ) {
+      thankYouCards.push( `Thank you, ${names[i]}, for the wonderful ${event} gift!` )
+    }
+  return thankYouCards
 }
 
-writeCards(gifts);
+function countDown(i){
+  while ( i > 0) {
+    console.log(i);
+    i -= 1;
+  }
+  console.log(i);
+}
